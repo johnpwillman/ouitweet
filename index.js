d5df3bb3610d1e5c449a68af041eb7be42703dea
John Willman <johnpwillman@gmail.com>
2016-08-12 21:33:09 -0700

    Grab recent twitter statuses at /timeline/:user

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var Twitter = require('twitter');
+
 var express = require('express');
 var app = express();
 
@@ -11,6 +13,29 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+// Helper functions
+function statuses_user_timeline(screen_name, resstream) {
+  var client = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  });
+
+  client.get('statuses/user_timeline', {screen_name: screen_name}, function(error, tweets, response) {
+    //console.log(tweets);
+    for (i = 0; i < tweets.length; i++) {
+      resstream.write(tweets[i].created_at + '\n');
+      resstream.write('[@' + tweets[i].user.screen_name + ']:\n');
+      resstream.write(tweets[i].text + '\n\n');
+    }
+    resstream.end();
+  });
+}
+
+
+
+
 // Page routes
 app.get('/', function(request, response) {
   response.render('pages/index');
@@ -18,6 +43,13 @@ app.get('/', function(request, response) {
 
 app.get('/helloworld', function(request, response) {
   response.send("hello world");
+});
+
+app.get('/timeline/:user', function(request, response) {
+
+  statuses_user_timeline(request.params.user, response);
+
+  //response.send(request.params.user);
 });
 
 // Run the application
